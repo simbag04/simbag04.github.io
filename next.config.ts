ed091970e9897fcd18f1a03de468454d9361b310
Simran Bagaria <sbagaria@Mac.lan>
2025-07-12 18:36:45 -0700

    bug

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/simbag04.github.io",
   images: {
     unoptimized: true,
   }
